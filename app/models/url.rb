096226502dad6609f027f55644708a52a3f4af64
catsinspacesuits <simunro@hotmail.co.uk>
2021-03-31 16:56:24 +0200

    Add format validation tests and refactor regex in validation

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,6 +1,6 @@
 class Url < ApplicationRecord
   before_validation :generate_slug
-  validates :original_url, format: URI::regexp(%w[http https])
+  validates :original_url, format: { with: /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/ix, message: "Please use a regular format" }
   validates_presence_of :short_url, :original_url
 
   def generate_slug
--- spec/model/url_spec.rb
+++ spec/model/url_spec.rb
@@ -10,34 +10,51 @@ require 'rails_helper'
 
 
 RSpec.describe Url do 
-  before(:all) do
-    @url = Url.create(original_url: "https://www.google.com/search?q=panda&sxsrf=ALeKk02bMVFQGDfaExjg3liAGJ6NNUbtWA161715sA")
-  end
-  # it 'saves an original url' do 
-  #   expect(@url.original_url).to eq("https://www.google.com/search?q=panda&sxsrf=ALeKk02bMVFQGDfaExjg3liAGJ6NNUbtWA161715sA")
-  # end
-  # it 'saves a slug' do
-  #   expect(@url.short_url).to be_truthy
-  # end
 
   # Validations
-   it 'is valid with valida attributes' do
-    expect(@url).to be_valid
+   it 'is valid with valid attributes' do
+    url = Url.create(original_url: "https://www.google.com/search?q=panda&sxsrf=ALeKk02bMVFQGDfaExjg3liAGJ6NNUbtWA161715sA")
+    expect(url).to be_valid
   end
 
   it "is not valid without an original_url" do
-    url = Url.new(original_url: nil)
+    url = Url.create(original_url: nil)
     expect(url).to_not be_valid
   end
 
-  it "is not valid without a short_url" do
-    url = Url.new(short_url: nil)
-    expect(url).to_not be_valid
-  end
+  context "when user input has correct format" do
+    it "is not valid with wrong protocol" do
+      url = Url.create(original_url: "https://https://google.com")
+      expect(url).to_not be_valid 
+    end
 
-  it 'is invalid if the short_url is not unique' do
-    same_url = Url.create
-    expect(same_url).to be_invalid
-  end
+    it "is not valid without domains" do
+      url = Url.create(original_url: "https://")
+      expect(url).to_not be_valid 
+    end
 
+    it "is not valid without top-level domain" do
+      url = Url.create(original_url: "https://google")
+      expect(url).to_not be_valid 
+    end
+
+    it "is valid with www" do
+      url = Url.create(original_url: "https://www.google.com")
+      expect(url).to be_valid 
+    end
+
+    it "is valid with http" do
+      url = Url.create(original_url: "https://www.google.com")
+      expect(url).to be_valid 
+    end
+
+    it "is valid with https" do
+      url = Url.create(original_url: "http://google.com")
+      expect(url).to be_valid 
+    end
+
+
+
+
+  end
 end 
