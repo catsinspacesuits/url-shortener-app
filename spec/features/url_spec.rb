0de970dbe90f8c9244a004cda13d5f074005e221
catsinspacesuits <simunro@hotmail.co.uk>
2021-04-01 04:20:01 +0200

    Add basic styles to views

--- spec/features/url_spec.rb
+++ spec/features/url_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.feature "Generate short url", type: :feature do
   scenario "With valid input" do
     visit "/urls/new"
-    fill_in("Please use format", with: 'https://test.com/testing')
+    fill_in('original_url', with: 'https://test.com/testing')
     click_button "create shortened url!"
 
     expect(page).to have_text("Here is your shortened URL!")
@@ -11,7 +11,7 @@ RSpec.feature "Generate short url", type: :feature do
 
   scenario "With invalid input" do
     visit "/urls/new"
-    fill_in("Please use format", with: 'https://test')
+    fill_in("original_url", with: 'https://test')
     click_button "create shortened url!"
 
     expect(page).to have_text("Please use a regular format")
